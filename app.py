9f615525774ca1862491bbed45843387842b109b
EmpProjects <projectsportal.apss@gmail.com>
2024-12-09 22:30:17 +0530

    commit all files

--- app.py
+++ app.py
@@ -1,82 +1,48 @@
+import numpy as np
+from flask import Flask, request, render_template
 import pickle
-import pandas as pd
-from flask import Flask, request, jsonify, render_template
 
+#Create an app object using the Flask class. 
 app = Flask(__name__)
 
-# Load the pre-trained Random Forest model
-rf_model = pickle.load(open('rf_model.sav', 'rb'))
+#Load the trained model. (Pickle file)
+model = pickle.load(open('models/model.pkl', 'rb'))
 
+#Define the route to be home. 
+#The decorator below links the relative route of the URL to the function it is decorating.
+#Here, home function is with '/', our root directory. 
+#Running the app sends us to index.html.
+#Note that render_template means it looks for the file in the templates folder. 
+
+#use the route() decorator to tell Flask what URL should trigger our function.
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
-    # Log the received JSON data
-    print("Received JSON data:", request.json)
-    
-    if not request.json or 'data' not in request.json:
-        return jsonify({'error': 'Invalid input, please provide data in JSON format'}), 400
-    
-    data = request.json['data']
-    
-    # Log the parsed data and its keys
-    print("Parsed data:", data)
-    print("Keys in parsed data:", list(data.keys()))
-    
-    # Define the required keys
-    required_keys = ['Gender', 'AGE', 'Height_cm', 'Weight_kg', 'BMI', 'Obesity_Class']
-    
-    # Check for missing keys
-    for key in required_keys:
-        if key not in data:
-            return jsonify({'error': f'Missing key: {key}'}), 400
-    
-    try:
-        input_data = [float(data[key]) for key in required_keys]
-    except ValueError:
-        return jsonify({'error': 'Invalid input types, please ensure all inputs are numeric'}), 400
-    
-    output = rf_model.predict([input_data])[0]
-
-    # Interpret the prediction
-    if output < 20:
-        vitamin_d_status = "Deficient"
-    elif output < 30:
-        vitamin_d_status = "Insufficient"
-    elif output <= 100:
-        vitamin_d_status = "Sufficient"
-    else:
-        vitamin_d_status = "Upper Safety Limit"
-
-    return jsonify({'Vitamin_D_Level': output, 'Status': vitamin_d_status})
-
-@app.route('/predict', methods=['POST'])
+#You can use the methods argument of the route() decorator to handle different HTTP methods.
+#GET: A GET message is send, and the server returns data
+#POST: Used to send HTML form data to the server.
+#Add Post method to the decorator to allow for form submission. 
+#Redirect to /predict page with the output
+@app.route('/predict',methods=['POST'])
 def predict():
-    data = [float(x) for x in request.form.values()]
-    
-    if len(data) != 6:
-        return render_template("home.html", prediction_text="Invalid input data. Please provide all six inputs.")
 
-    output = rf_model.predict([data])[0]
+    int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
+    features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
+    prediction = model.predict(features)  # features Must be in the form [[a, b]]
 
-    # Interpret the prediction
-    if output < 20:
-        vitamin_d_status = "Deficient"
-    elif output < 30:
-        vitamin_d_status = "Insufficient"
-    elif output <= 100:
-        vitamin_d_status = "Sufficient"
-    else:
-        vitamin_d_status = "Upper Safety Limit"
+    output = round(prediction[0], 2)
 
-    return render_template("home.html", prediction_text=f"Predicted Vitamin D Level: {output} ({vitamin_d_status})")
+    return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
+
+
+#When the Python interpreter reads a source file, it first defines a few special variables. 
+#For now, we care about the __name__ variable.
+#If we execute our code in the main program, like in our case here, it assigns
+# __main__ as the name (__name__). 
+#So if we want to run our code right here, we can check if __name__ == __main__
+#if so, execute it here. 
+#If we import this file (module) to another file then __name__ == app (which is the name of this python file).
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
-
+    app.run()
